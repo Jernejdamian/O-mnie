28b7d6fdf827921da1699ddb2eafa044bb37dbb5
Damian Salawa <damiandv20@o2.pl>
2023-01-08 17:36:50 +0100

    body and fspan transferred to toggleBackgoundColor

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 {
     const button = document.querySelector(".button");
-    const body = document.querySelector(".body");
-    const fspan = document.querySelector(".fspan");
 
     const toggleBackgroundColor = () => {
+
+        const body = document.querySelector(".body");
+        const fspan = document.querySelector(".fspan");
+
         body.classList.toggle("changeBody");
         if (fspan.innerText === "WhiteGold") {
             fspan.innerText = "BlackGold";
